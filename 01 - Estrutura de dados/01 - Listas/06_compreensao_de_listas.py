a48064adb4472d0cb531958ddcfb27038460fa24
gui-anacleto <guilherme_fa@outlook.com>
2025-03-27 01:50:11 -0300

    Conteudo completo

--- 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
+++ 01 - Estrutura de dados/01 - Listas/06_compreensao_de_listas.py
@@ -1,9 +1,26 @@
-# Filtrar lista
+# Filtrar lista - python version
 numeros = [1, 30, 21, 2, 9, 65, 34]
 pares = [numero for numero in numeros if numero % 2 == 0]
+print(pares)
+
+# Filtro lista versao 1
+numeros = [1, 30, 21, 2, 9, 65, 34]
+pares = []
+
+for numero in numeros:
+    if numero % 2 == 0:
+        pares.append(numero)
 print(pares)
 
 # Modificar valores
 numeros = [1, 30, 21, 2, 9, 65, 34]
 quadrado = [numero**2 for numero in numeros]
+print(quadrado)
+
+# Modificar valores versao 1
+numeros = [1, 30, 21, 2, 9, 65, 34]
+quadrado = []
+
+for numero in numeros:
+    quadrado.append(numero**2)
 print(quadrado)
